6bc3ab0a68d58c1927ea2072be3b5ac128a0a05b
beginerX0 <dan123nik@gmail.com>
2025-02-14 18:49:14 +0300

    Добавлено логгирование и POST-Метод

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -1,4 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
+using NLog;
+using NLog.Web;
 
 namespace nikolaevdaniilKt_31_22.Controllers
 {
@@ -21,6 +23,7 @@ namespace nikolaevdaniilKt_31_22.Controllers
         [HttpGet(Name = "GetWeatherForecast")]
         public IEnumerable<WeatherForecast> Get()
         {
+            _logger.LogError("Method was called");
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
             {
                 Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
@@ -28,6 +31,16 @@ namespace nikolaevdaniilKt_31_22.Controllers
                 Summary = Summaries[Random.Shared.Next(Summaries.Length)]
             })
             .ToArray();
+        }
+
+        [HttpPost(Name = "AddNewSummary")]
+        public string[] AddNewSummay(string newSummary)
+        {
+            _logger.LogError("New method was called");
+
+            var list = Summaries.ToList();
+            list.Add(newSummary);
+            return list.ToArray();
         }
     }
 }
